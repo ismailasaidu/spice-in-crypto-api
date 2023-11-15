9ad98c848aa0d61a71be79cd0f8115779ae6cbf4
Ismaila Saidu <ismailasaidu997@gmail.com>
2023-11-15 15:06:03 +0100

    first commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/api/", (req, res) => {
+app.get("/api/test", (req, res) => {
   res.semd("testing");
 });
 
